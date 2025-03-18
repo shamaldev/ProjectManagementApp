3c070b4041ce17c099021f8f33646c8791dd1f14
Mohammed Shamal <Mohammed.Shamal@novigo.com>
2025-03-18 09:22:50 +0530

    initialize React project with Vite, Tailwind CSS, and ESLint setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
