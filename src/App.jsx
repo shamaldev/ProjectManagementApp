cfb10582e0abf47a6a614e3f6a11f9a3e72303c7
Mohammed Shamal <Mohammed.Shamal@novigo.com>
2025-03-18 14:41:32 +0530

    implement project management features with project creation and selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+// import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import NewProject from "./components/NewProject";
 function App() {
+  const [projectsState,setProjectsState] = useState({
+    selectedProjectId :undefined,
+    projects :[]
+  })
+  function handleStartAddProject(){
+    setProjectsState((prevState)=>{
+      return {
+        ...prevState,
+        selectedProjectId : null
+      }
+    })
+  }
+function handleAddProject(projectData) {
+  setProjectsState((prevState)=>
+  {
+    const newProject ={
+      ...projectData,
+      id:Math.random()
+
+    }
+    return {
+      ...prevState,
+      selectedProjectId:undefined,
+      projects:[...prevState.projects,newProject]
+    }
+  })
+}
+console.log(projectsState);
+
+function handleCancelAddProject() {
+  setProjectsState((prevState)=>{
+    return {
+      ...prevState,
+      selectedProjectId : undefined
+    }
+  })
+}
+
+
+let content;
+if (projectsState.selectedProjectId === null) {
+  content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
+}else if (projectsState.selectedProjectId === undefined) {
+  content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+}
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+     <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+     {/* <NewProject /> */}
+    {content}
+    </main>
   );
 }
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// eslint-disable-next-line react/prop-types
+export default function Button({children,...props}) {
+  return (
+    <>
+        {/* eslint-disable-next-line react/prop-types */}
+        <button {...props} className='px-4 py-2 tex-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100'>
+           {children}
+        </button>
+    </>
+   
+  )
+}
